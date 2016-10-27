007dfd6f754a33ae61739946e271ad5cf20c49c7
Charlie Cuddy <charliecuddy4@gmail.com>
2016-10-27 21:14:01 +0000

    remove prime squares from the primeNumberArray and set up onclick button to cycle through... BUG: prime * prime are still being added to array

--- primeNumbers/script.js
+++ primeNumbers/script.js
@@ -9,23 +9,50 @@
 // puts 1 to 8000 in an array
 
     var allNumbersArray = [0, 1]
-    var primeNumbersArray = [2, 3, 5, 7]
-        for (var i = 2; i < 100; i++) {
+    var primeNumbersArray = ['', 2, 3, 5, 7]
+    function findPrimes() {        
+        for (var i = 2; i < 1000; i++) {
         allNumbersArray.push(i);
-        if (i % 2 !== 0 && i % 3 !== 0 && i % 5 !== 0 && i % 7 !== 0) {
-        primeNumbersArray.push(i);  
-    }    
-}
+            if (i % 2 !== 0 && i % 3 !== 0 && i % 5 !== 0 && i % 7 !== 0 && i % Math.sqrt(i) !==0) {
+            primeNumbersArray.push(i);  
+            }    
+        }
+    }
+    findPrimes();
 
-var nextPrime = document.getElementById('nextPrime');
-nextPrime.addEventListener('click', function(){
-   var primeNumberArray = document.getElementById('currentPrimeNumber');
-    for (i = 0; i < primeNumbersArray.length; i++) {
-        primeNumberArray.innerHTML = primeNumbersArray[i];
-    } 
-});
+
+// if (primeNumbersArray[i] divides into i then do not put it in primeNumbersArray)
+
+// var nextPrime = document.getElementById('nextPrime');
+// nextPrime.addEventListener('click', function(){
+//   var primeNumberArray = document.getElementById('currentPrimeNumber');
+//     for (i = 0; i < primeNumbersArray.length; i++) {
+//         primeNumberArray.innerHTML = primeNumbersArray[i];
+//     } 
+// });
 
     
+
+
+
+var myIndex = 1;
+var currentPrimeNumber = document.getElementById('currentPrimeNumber');
+
+currentPrimeNumber.innerHTML = primeNumbersArray[0]; //Print first value of array right away.
+
+function nextPrimeClick() {
+  if (myIndex < primeNumbersArray.length) {
+     currentPrimeNumber.innerHTML = primeNumbersArray[myIndex];
+     myIndex++;
+  }
+  else {
+     currentPrimeNumber.innerHTML = "CONGRATS! You made it through first 1000 Prime Numbers! Your finger should be sore. Go outside and play!";   
+  }
+};
+
+
+
+
 
 
 // BUG: a prime * prime is being put into the primeNumbersArray --> obviously not a prime number
