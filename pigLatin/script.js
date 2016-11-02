ac6661fcffbab0d9a1ea4e4eca9226d0a5abb8a1
Charlie Cuddy <charliecuddy4@gmail.com>
2016-11-02 04:54:33 +0000

    establish input for translate, and save input as an object, displaying the li but not phrase

---
+++ pigLatin/script.js
@@ -0,0 +1,44 @@
+// Pig Latin - Pig Latin is a game of alterations played on the English language game. 
+// To create the Pig Latin form of an English word the initial consonant sound is 
+// transposed to the end of the word and an ay is affixed 
+// (Ex.: "banana" would yield anana-bay). 
+
+var pigLatin = {
+  phrase: [],
+  translatePhrase: function(userPhrase) {
+    this.phrase.push({
+      userPhrase: userPhrase,   //first is property, second is param
+    });
+  }
+};
+
+var handlers = {
+  translatePhrase: function() {
+    var addPhraseToTranslate = document.getElementById('addPhraseToTranslate');
+    if(addPhraseToTranslate.value !== '') {
+      pigLatin.translatePhrase(addPhraseToTranslate.value);
+      addPhraseToTranslate.value = '';
+      view.displayPigLatin();
+    } else {
+      alert("You didn't enter anything to translate!");
+    }
+  }
+};
+
+
+var view = {
+  displayPigLatin: function() {
+    var pigLatinUl = document.querySelector('ul');
+    pigLatinUl.innerHTML = '';
+  
+    pigLatin.phrase.forEach(function(userPhrase, position) {
+        var pigLatinLi = document.createElement('li');
+        var displayPigLatinOnScreen = '';
+        pigLatin.phrase;
+      
+        pigLatinLi.id = position;
+        pigLatinLi.textContent = displayPigLatinOnScreen;
+        pigLatinUl.appendChild(pigLatinLi);
+    });
+  }
+};
