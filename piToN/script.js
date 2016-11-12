acca8f9d185ab8920015fdf8c3a895041027b4c7
Charlie Cuddy <charliecuddy4@gmail.com>
2016-11-12 05:29:07 +0000

    round the decimal shown

--- piToN/script.js
+++ piToN/script.js
@@ -2,8 +2,20 @@ function decimalOfPi(){
         var pi = Math.PI;
         var piLocation = document.getElementById('showPi');
         var nthDigitOfPi = document.getElementById('nthDigitOfPi');
-        piLocation.innerHTML = pi.toFixed(nthDigitOfPi.value);
+        piLocation.innerHTML = pi.toFixed(nthDigitOfPi.value.round);
     }   
 
-// 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825
+//Things to Improve: 
+//Round the Decimal
+//Use longer decimal as shown below
+    //code below will show the nth term but nothing before it
+
+// function moreDecimal() {
+//     var pi = "3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825"
+//     var piLocation = document.getElementById('showPi');
+//     var nth = document.getElementById('nthDigitOfPi');
+//     var cut = pi.charAt(nth.value);
+//     piLocation.innerHTML = cut;
+// }
+
 
