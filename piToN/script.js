b2b9ec6f1259c72ce596a1fcd856468160341328
Charlie Cuddy <charliecuddy4@gmail.com>
2016-11-12 04:39:42 +0000

    add oninput to fire user input number and connect it to value of pi

--- piToN/script.js
+++ piToN/script.js
@@ -1,19 +1,9 @@
-var pi = Math.PI;
-var nthDigitOfPi = document.getElementById('nthDigitOfPi');
-var piLocation = document.getElementById("showPi");
-piLocation.innerHTML = (pi).toFixed(10);
-
-var piInfo = {
-    piDigitsArray: [],
-    getDigit: function(nthDigitOfPi){
-        this.piDigitsArray.push({
-            
-        });
+function decimalOfPi(){
+        var pi = Math.PI;
+        var piLocation = document.getElementById('showPi');
+        var nthDigitOfPi = document.getElementById('nthDigitOfPi');
+        piLocation.innerHTML = pi.toFixed(nthDigitOfPi.value);
     }   
-}
-
-
-
 
 // 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825
 
